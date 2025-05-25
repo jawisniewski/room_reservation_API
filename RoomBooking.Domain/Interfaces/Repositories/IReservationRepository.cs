3617bdaf04f957d5f8e424cf7a04b4d5263ae1d3
Wisniewski <jwisniewski994@gmail.com>
2025-05-25 13:55:26 +0200

    warstwa domenowa + struktura projektowa

---
+++ RoomBooking.Domain/Interfaces/Repositories/IReservationRepository.cs
@@ -0,0 +1,18 @@
+﻿using RoomsReservation.Domain.Entitis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomsReservation.Domain.Interfaces.Repositories
+{
+    public interface IReservationRepository
+    {
+        public Task<Guid> Create(Reservation reservation);
+        public Task<bool> Update(Reservation reservation);
+        public Task<bool> Delete(Guid roomId);
+        public Task<Reservation?> GetById(Guid id);
+        public Task<bool> UserHasReservation(Guid userId, DateTime from, DateTime to, Guid? excludedReservationId);
+    }
+}
---
+++ RoomBooking.Domain/Services/RoomReservationService.cs
@@ -0,0 +1,90 @@
+﻿using RoomBooking.Domain.Entitis.Room;
+using RoomBooking.Domain.Exceptions;
+using RoomsReservation.Domain.Entitis;
+using RoomsReservation.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBooking.Domain.Services
+{
+    public class RoomReservationService
+    {
+        private readonly IReservationRepository _reservationRepository;
+        private readonly IRoomRepository _roomRepository;
+
+        public RoomReservationService(IReservationRepository reservationRepository, IRoomRepository roomRepository)
+        {
+            _reservationRepository = reservationRepository;
+            _roomRepository = roomRepository;
+        }
+
+        public async Task<Guid> ReserveRoom(Guid roomId, DateTime from, DateTime to, Guid userId)
+        {
+            var room = await _roomRepository.GetByIdAsync(roomId)
+                ?? throw new DomainException("Room not found");
+
+            room.IsReservationWithinLimits(from, to);
+
+            var isAvailable = await _roomRepository.IsAvailableAsync(roomId, from, to);
+
+            if (!isAvailable)
+            {
+                throw new DomainException("Room is not available");
+            }
+
+            var userHasAReservation = await _reservationRepository.UserHasReservation(userId, from, to, null);
+
+            if (userHasAReservation)
+            {
+                throw new DomainException("User already has a reservation during this time");
+            }
+
+            var reservation = new Reservation(from, to, roomId, userId);
+
+            var result = await _reservationRepository.Create(reservation);
+
+            return result;
+        }
+
+        public async Task<bool> UpdateReservation(Guid reservationId, Guid roomId, DateTime from, DateTime to, Guid userId)
+        {
+            var existingReservation = await _reservationRepository.GetById(reservationId)
+                ?? throw new DomainException("Reservation not found");
+
+            var isAvailable = await _roomRepository.IsAvailableAsync(roomId, from, to, reservationId);
+
+            if (!isAvailable)
+            {
+                throw new DomainException("Room is not available");
+            }
+
+            var userHasAReservation = await _reservationRepository.UserHasReservation(userId, from, to, reservationId);
+
+            if (userHasAReservation)
+            {
+                throw new DomainException("User already has a reservation during this time");
+            }
+
+            existingReservation.Update(from, to, roomId, userId);
+
+            var result = await _reservationRepository.Update(existingReservation);
+
+            return result;
+        }
+
+        public async Task<bool> CancelReservation(Guid reservationId, Guid userId)
+        {
+            var reservation = await _reservationRepository.GetById(reservationId)
+                ?? throw new DomainException("Reservation not found");
+
+            reservation.IsAuthorized(userId);
+
+            var result = await _reservationRepository.Delete(reservationId);
+
+            return result;
+        }
+    }
+}
